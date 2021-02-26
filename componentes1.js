05bfd180c9e38f36c6034ab5f1139b266c924323
davidperez-curso <pruebagithub1@gmail.com>
2021-02-26 15:09:19 -0300

    1er commit

---
+++ componentes1.js
@@ -0,0 +1,12 @@
+Vue.component('componente1',{
+    template:
+    /*html*/`
+    <div>
+        <h1>Este es un ejemplo de componente 1</h1>
+    </div>
+    `
+})
+
+const comp1 = new Vue ({
+    el: '#comp1'
+})
---
+++ componentes2.js
@@ -0,0 +1,12 @@
+Vue.component('componente2',{
+    template:
+    /*html*/`
+    <div>
+        <h1>Este es un ejemplo de componente 2</h1>
+    </div>
+    `
+})
+
+const comp2 = new Vue ({
+    el: '#comp2'
+})
